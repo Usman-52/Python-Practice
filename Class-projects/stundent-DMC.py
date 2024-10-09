e3885e27e7ba9a06a5a2651fa29c18a2ad48e018
Usman-52 <usman914.764@gmail.com>
2024-10-09 13:07:38 +0500

    add another small project

---
+++ Class-projects/stundent-DMC.py
@@ -0,0 +1,16 @@
+std_1_DMC = {
+    "Roll No": "48",
+    "Name": "Muhammad Usman",
+    "Registration No": "AUP-2021-48",
+    "Semester": 5,
+    "Subjects Marks": {
+        "Professional Practice": 90,
+        "Web Design & Development": 59,
+        "Economics": 68,
+        "AI": 67,
+        "Networking": 74
+    },
+    "GPA": 3.1
+}
+
+print(std_1_DMC["Subjects Marks"])
